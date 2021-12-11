c1e48e247912fa76cbe8bbfc14d7cea488ac269c
Shin Yamaga <shinyamaga@ShinnoMacBook-Air.local>
2021-12-11 18:43:34 +0900

    投稿機能の追加

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from blog.views import PostList, PostDetail
+from blog.views import PostList, PostDetail, PostCreateView
 
 urlpatterns = [
     path("", PostList.as_view(), name=""),
-    path("post/<int:pk>/", PostDetail.as_view(), name="post_detail")
+    path("post/<int:pk>/", PostDetail.as_view(), name="post_detail"),
+    path("post_create", PostCreateView.as_view(), name="post_create"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,34 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 from django.core.exceptions import PermissionDenied
 
 from blog.models import Post
+
+from . import forms
 # Create your views here.
 
+
 class PostList(ListView):
-  model = Post
-  template_name = "blog/index.html"
-  context_object_name = "posts"
+    model = Post
+    template_name = "blog/index.html"
+    context_object_name = "posts"
 
 
 class PostDetail(DetailView):
-  model = Post
-  context_object_name = "post"
+    model = Post
+    context_object_name = "post"
 
-  def get_object(self):
-    post = super().get_object()
-    if post.is_published:
-      return post
-    else:
-      raise PermissionDenied
+    def get_object(self):
+        post = super().get_object()
+        if post.is_published:
+            return post
+        else:
+            raise PermissionDenied
+
+
+class PostCreateView(CreateView):
+
+    model = Post
+    form_class = forms.PostCreateFrom
+    success_url = reverse_lazy('')
